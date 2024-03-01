cc51eb816786dee3d2c276e068042d98e5c53542
Marian-Sebastian Radu <radu.marian.2431.1234@gmail.com>
2024-03-01 09:41:54 +0200

    lab1_ex2.cpp

--- LAB1/lab1_ex2.cpp
+++ LAB1/lab1_ex2.cpp
@@ -1,28 +1,22 @@
 #include<stdio.h>
 #include<cstring>
 
+char C[100][100], s[256], aux[100], *p;
+int count, i, j;
 int main()
 {
-    char C[100][100];
-    char s[256] , *p;
-    strcpy(s,"");
     printf("Enter a phrase: ");
-    scanf("%[^\n]s",s);//!!
+    scanf("%[^\n]s",s);//!!conditie de oprire la newline
     p = strtok(s," ");
-    //printf("%s\n",p);
-    int count = 0;
-    //cout<<"da"<<endl;
     while(p!=NULL)
     {   
         strcpy(C[count++],p);
         p = strtok(NULL," ");
     }
-    int i , j;
     for(i=0;i<count;i++)
         for(j=i+1;j<=count;j++)
             if(strlen(C[i])<strlen(C[j]))
             {
-                 char aux[100];
                         strcpy(aux,C[j]);
                         strcpy(C[j],C[i]);
                         strcpy(C[i],aux);   
@@ -30,16 +24,11 @@ int main()
             else
                 if(strcmp(C[j],C[i])>0 && strlen(C[j])==strlen(C[i]))
                     {
-                        char aux[100];
                         strcpy(aux,C[j]);
                         strcpy(C[j],C[i]);
                         strcpy(C[i],aux);
                     }
     for(int i = 0;i<=count;i++)
         printf("%s\n",C[i]);
-        
-
-    
-    return 0;
-        
+    return 0;        
 }
