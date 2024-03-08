346b6c426cabaf076878e4a8df283db503b0c696
Marian-Sebastian Radu <radu.marian.2431.1234@gmail.com>
2024-03-08 22:52:33 +0200

    exercitiul_2

--- LAB2/FunctionGlobals.h
+++ LAB2/FunctionGlobals.h
@@ -1,6 +1,6 @@
 #pragma once
 #include"Student.h"
-int byName(student* M, student* N);
+int byName(student* M , student* N);
 int byMath(student* M, student* N);
 int byEnglish(student* M, student* N);
 int byHistory(student* M, student* N);
