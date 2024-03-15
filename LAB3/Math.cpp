151a02c6d7407e702ab1ecdbd0e881979ddd3a64
Marian-Sebastian Radu <radu.marian.2431.1234@gmail.com>
2024-03-15 17:26:42 +0200

    Update Math.cpp

--- LAB3/Math.cpp
+++ LAB3/Math.cpp
@@ -47,52 +47,42 @@ int Math::Add(int count, ...)//sums up a list of integers
 }
 
 
-char* Math::Add(const char* x, const char* y)//concateneaza cele 2 siruri
+char* Math::Add(const char* x, const char* y)
 {	
 	if (x == nullptr || y==nullptr)
 		return nullptr;
+
+
+	//Daca era de facut concatenarea celor 2 siruri
+	 
 	//const int m = strlen(x) + strlen(y)+1;
 	//const auto s = new char[m];
 	//strcpy_s(s, m, x);
 	//strcat_s(s, m, y);
 	//return s;
-	//
-
-	//le facem int pe x si y
-	int number1 = 0, i;
-	for (i = 0; i < strlen(x); i++)
-		number1 = number1 * 10 + x[i] - '0';
-	int number2 = 0;
-	for (i = 0; i < strlen(y); i++)
-		number2 = number2 * 10 + y[i] - '0';
-
-	//facem suma
-	number1 += number2;
-
-	//verificam cate cifre are numarul
-	int aux = number1, nr = 0;
-	while (aux)
-	{
-		aux /= 10;
-		nr++;
-	}
-
-
-	//alocam dinamic un vector de char si convertim int in char
-	char* xx = new char[nr];
-	xx[nr] = '\0';
-	//cout << "Numarul de cifre: " << nr << '\t' << number1 << '\t' << strlen(xx) << endl;
-	nr = 0;
-	while (number1)
-	{
-		xx[nr++] = number1 % 10 + '0';
-		number1 /= 10;
-	}
-	//xx[nr] = '\0';
-	_strrev(xx);//mai sus s-a convertit oglinditul lui number1
 	
-	return xx;
+
+
+	if (strlen(x) > strlen(y))
+		swap(x, y);	
+	char*  = new char[strlen(y) + 1];
+	int zz = 0 , xx = strlen(x) - 1 , yy = strlen(y) - 1 , exces = 0;
+	while (xx>-1)
+	{	
+		int a = x[xx--] - '0' + y[yy--] - '0' + exces;
+		exces = (a > 9) ? 1 : 0;
+		z[zz++] = a % 10 + '0';
+	}
+	while (yy>-1)
+	{
+		int a = y[yy--] - '0' + exces;
+		exces = (a > 9) ? 1 : 0;
+		z[zz++] = a % 10 + '0';
+	}
+	if (exces)
+		z[zz++] = exces + '0';
+	z[zz] = '\0';
+	_strrev(z);
+	return z;
 	
 }
-
-
