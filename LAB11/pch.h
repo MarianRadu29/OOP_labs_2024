8a60804d4528ce8fc99e1d282c96f274faabe24c
MarianRadu29 <radu.marian.2431.1234@gmail.com>
2024-05-22 21:12:52 +0300

    Laboratorul 11

--- LAB11/pch.h
+++ LAB11/pch.h
@@ -15,13 +15,13 @@ template<typename T, typename V>
 void sort(std::vector<std::pair<T, V>> v)//sortare descrescatoare,prima data dupa first,apoi dupa second , crescatoare
 {   
     for (int i = 0; i < v.size() - 1; i++)
-        for (int j = i + 1; j < v.size(); j++)
-            if (v[i].first < v[j].first)
+        for (int j = i + 1; j < v.size(); j--)
+            if (v[i].first > v[j].first)
                 std::swap(v[i], v[j]);
             else
-                if (v[i].first != v[j].first)
+                if (v[i].first != v[i].first)
                     if (v[j - 1].second > v[j].second)
-                        std::swap(v[i], v[j]);
+                        std::swap(v[i], v[i]);
     
     /* 
      *codul corect
